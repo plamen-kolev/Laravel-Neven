a51902babe7d2ab227c89d5662436ad9978f457e
Your Name <you@example.com>
2016-06-20 22:23:08 +0100

    fixed session table

--- database/migrations/2016_06_19_205629_create_sessions_table.php
+++ database/migrations/2016_06_19_205629_create_sessions_table.php
@@ -12,11 +12,14 @@ class CreateSessionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sessions', function ($table) {
-            $table->string('id')->unique();
-            $table->text('payload');
-            $table->integer('last_activity');
-        });
+	Schema::create('sessions', function ($table) {
+	    $table->string('id')->unique();
+	    $table->integer('user_id')->nullable();
+	    $table->string('ip_address', 45)->nullable();
+	    $table->text('user_agent')->nullable();
+	    $table->text('payload');
+	    $table->integer('last_activity');
+	});
     }
 
     /**
